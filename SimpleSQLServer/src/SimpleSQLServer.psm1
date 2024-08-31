058a8c5fd5e40fa4d5973324c2311f8bebe48afc
Kyle Norris <r.kyle.norris@gmail.com>
2024-08-30 21:42:37 -0400

    created Invoke-SQLSelectQuery

--- SimpleSQLServer/src/SimpleSQLServer.psm1
+++ SimpleSQLServer/src/SimpleSQLServer.psm1
@@ -56,6 +56,69 @@ function New-SQLConnection {
 
 # TODO create sql select function, CRUD functions, bulk insert
 
+function Invoke-SQLSelectQuery {
+    [CmdletBinding()]
+    param(
+        # Parameter help description
+        [Parameter(Mandatory,
+        ParameterSetName='FromFile')]
+        [Parameter(Mandatory,
+        ParameterSetName='FromString')]
+        [system.data.SqlClient.SQLConnection]
+        $SQLConnection,
+        # Parameter help description
+        [Parameter(Mandatory,
+        ParameterSetName='FromString')]
+        [string]
+        $SelectQueryText,
+        # Parameter help description
+        [Parameter(Mandatory,
+        ParameterSetName='FromFile')]
+        [string]
+        $SQLQueryFile,
+        [Parameter(Mandatory=$False,
+        ParameterSetName='FromFile')]
+        [Parameter(Mandatory=$False,
+        ParameterSetName='FromString')]
+        [switch]
+        $CloseConnection
+    )
+
+    
+    $query = [string]::Empty
+    if($PSCmdlet.ParameterSetName -eq 'FromString'){
+        if(-not([string]::IsNullOrWhiteSpace($SelectQueryText) -and [string]::IsNullOrEmpty($SelectQueryText))){
+            $query = $SelectQueryText
+        }else{
+            throw [System.Management.Automation.PSArgumentNullException] "query string passed null or empty"
+        }
+        
+    }else{
+        if(Test-Path $SQLQueryFile){
+            $query = Get-Content $SQLQueryFile -Raw    
+        }else{
+            throw [System.IO.FileNotFoundException] "$SQLQueryFile not found."
+        }
+    }
+
+    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand($query, $SQLConnection)
+
+
+    if($SQLConnection.State -eq [System.Data.ConnectionState]::Closed){
+        $SQLConnection.Open()
+    }
+
+    $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter $sqlCommand
+    $dataset = New-Object System.Data.DataSet
+    $sqlAdapter.Fill($dataset) | Out-Null
+
+    if($CloseConnection){
+        $SQLConnection.Close()
+    }
+
+    return ,$dataset
+}
+
 
 # Export only the functions using PowerShell standard verb-noun naming.
 Export-ModuleMember -Function *-*
